a7a8ce0fdabe97d14e590cfdadd910b81991c23b
Yatish Katagar <yatish.katagar@siemens.com>
2021-01-14 19:05:32 +0530

    Shopping Page Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage.component';
-
-const HatsPage = () => <div>Hats Page;</div>;
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route path='/' exact component={HomePage} />
-				<Route path='/hats' component={HatsPage} />
+				<Route path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
 	);
